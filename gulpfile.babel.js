e26677c26eb122120f5396cd6f6b1a198e8bde52
misioq23 <mi23chal@gmail.com>
2019-03-11 23:18:13 +0100

    Karma works fine

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,11 +3,17 @@
 // Load plugins
 import gulp from 'gulp';
 import watchFiles from './config/gulp/watch';
-
+import { Server } from 'karma';
 import { browserSync, browserSyncJSON } from './config/gulp/serv';
 
 // define complex tasks
-const watch = gulp.parallel(watchFiles, browserSync);
-
+const karma = () => {
+	const server = new Server({
+		configFile: __dirname + '/config/karma/default.js',
+		singleRun: false
+	})
+	server.start()
+};
+const watch = gulp.parallel(watchFiles, browserSync, karma);
 export const json = gulp.parallel(watchFiles, browserSyncJSON);
 export default watch;
