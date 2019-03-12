88e8ea4fce2d92203545cd75506c65393e41cb34
misioq23 <mi23chal@gmail.com>
2019-03-12 18:40:35 +0100

    added gulp karma and edited code in gulpfile.babel

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,10 +3,10 @@
 // Load plugins
 import gulp from 'gulp';
 import watchFiles from './config/gulp/watch';
-import karma from './config/gulp/tests';
+import karmaTest from './config/gulp/tests';
 import { browserSync, browserSyncJSON } from './config/gulp/serv';
 
 // define complex tasks
-const watch = gulp.parallel(watchFiles, browserSync, karma);
+export const karma = gulp.parallel(watchFiles, browserSync, karmaTest);
 export const json = gulp.parallel(watchFiles, browserSyncJSON);
-export default watch;
+export default gulp.parallel(watchFiles, browserSync);
