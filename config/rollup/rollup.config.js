c9ef9c94d9b0cb140f4428ce39eb978a4d394001
Michal Pawluczuk <mi23chal@gmail.com>
2020-09-23 15:29:23 +0200

    chore: update rollup

--- config/rollup/rollup.config.js
+++ config/rollup/rollup.config.js
@@ -1,7 +1,6 @@
-import babel from 'rollup-plugin-babel';
-import replace from 'rollup-plugin-replace';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import config from '../config';
 
 const rollupConfig = {
@@ -12,15 +11,10 @@ const rollupConfig = {
 		sourcemap: true,
 	},
 	plugins: [
-		replace({
-			'process.env.NODE_ENV': '"production"',
-		}),
-		resolve({
-			mainFields: ['jsnext'],
-			browser: true,
-		}),
+		resolve(),
 		babel({
 			exclude: 'node_modules/**',
+			babelHelpers: 'bundled',
 			presets: [
 				[
 					'@babel/preset-env',
